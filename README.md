0a1f703fd6fe0ab60c90b5c26c118bb7f4cc9b06
Miguel <mconcha@ciencias.unam.mx>
2016-10-19 23:52:02 -0500

    Server running appropriately.

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
-# Practica09-Modelado20171
+# Practica10-Modelado20171
 
 __Clásico juego de Snake implementado con Pyqt4.__ 
 
-![captura de pantalla de 2016-10-12 19-56-33](https://cloud.githubusercontent.com/assets/13952922/19332975/df3120cc-90b6-11e6-8da8-8776c1ebe9e6.png)
 
-Entre otras cosas, permite: 
+El juego ahora permite comunicarse con con XMLRPCServer y hacer ciertas peticiones a funciones que fueron registradas en el servidor, a saber: 
 
-* Incrementar dinámicamente el número de celdas de la tabla.
-* Modificar la velocidad a la que se mueven la serpiente.
-* Notificar al jugador cuando comió o cuando la serpiente murió.
-* Pausar, reanudar y terminar el juego cuando se desee.
-* Llevar un contador del Highscore del juego, mientras la sesión no haya sido terminada por el usuario.
+* ping: Regresa la cadena __¡Pong!__
+* yo_juego: Crea una nueva serpiente que mete al juego, además de que se le asigna un identificador único (UUID) y un color en formato RGB aleatorio. Regresa un diccionario con esta información.
+* cambia_dirección: Permite modificar la dirección de una serpiente que se encuentea en el juego a partir de su identificador único dado como parámetro y un número que reprsenta la dirección (tiene que ser un movimiento válido de acuerdo con la dirección actual de la víbora, e.g, no puede moverse para arriba si se está moviento hacia abajo): 
+ * 0: Arriba
+ * 1: Derecha
+ * 2: Abajo
+ * 3: Izquierda
+* estado_del_juego: Regresa un diccionario que permite conocer la información más importante del juego, como es: 
+ * La espera del servidor (timeout).
+ * El número de columnas en la tabla.
+ * El número de filas/renglones en la tabla.
+ * Una lista de las víboras, en donde cada víbora a su vez se puede ver como un diccionario que contiene: 
+  * Su identificador único.
+  * Su diccionario de colores RGB
+  * Una lisa de tuplas que representan el camino de la serpiente, i.e., la lista de las posiciones (x,y) que ocupa la serpiente en la tabla.
 
--- servidor.py
+++ servidor.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
 import sys
+from xmlrpc.server import SimpleXMLRPCServer #Implementar el servidor.
 from PyQt4 import QtGui, QtCore, uic
 from random import randint #Para poder generar la celda en donde aparece la comida.
+import uuid #Crear un id único para cada víbora.
 
 class Serpiente():
     '''
@@ -9,12 +12,30 @@ class Serpiente():
     como parámetros. Falta todavía implementar que la serpiente se cree en un lugar donde
     no haya ya otra, pero por ahora no nos preocupa porque es un juego de un solo jugador.
     '''
-    def __init__(self, red, green, blue):
-        self.color = (red, green, blue)
-        self.casillas = [[3,0],[4,0],[5,0], [6,0], [7,0], [8,0], [9,0],[10,0], [11,0]] #La serpiente se representa como una lista de listas, donde cada una de las sublistas representa una celda que ocupa en la tabla.
+    def __init__(self):
+        self.id = str(uuid.uuid4())[:8] #Solamente nos tomamos la primer partd del id porque son muy largos.
+        red, green, blue = randint(0,255), randint(0,255), randint(0,255)
+        self.color = {"r": red, "g": green, "b": blue} #El diccionario que necesitaremos más adelante.
+        self.camino = [] #Esta será una lista de tuplas; como las tuplas no son mutables, tendremos que estar construyéndola siempre.
+        self.casillas = [] #Esta será la lista de las partes del cuerpo de la serpiente.
+        self.camino = []
         self.tam = len(self.casillas)
         self.direccion = "Abajo" #Bandera que servirá para poder dibujar correctamente a la serpiente y validar ciertos movimientos del usuario.
-        self.veces_ha_comido = 0
+        self.veces_ha_comido = 0 #Se necesita para poder tener el highscore.
+
+    def obtener_diccionario(self):
+        '''
+        Función que permite visualizar a una serpiente del juego
+        a partir de sus características principales: color, identificador y 
+        las casillas que ocupa.
+        '''
+        diccionario = dict()
+        diccionario = {
+            'id': self.id,
+            'camino': self.camino, 
+            'color': self.color
+        }
+        return diccionario #Será necesario para poder implementar la función que será llamada por el cliente.
 
 class VentanaServidor(QtGui.QMainWindow):
 
@@ -28,11 +49,14 @@ class VentanaServidor(QtGui.QMainWindow):
         uic.loadUi('servidor.ui', self) #Se carga el archivo hecho en QtDesigner.
         self.label_8.setStyleSheet("QLabel { color : #f442b0; }")
         self.pushButton_3.hide() #Este botón solamente aparece hasta que se comienza el juego.
+        self.pushButton.clicked.connect(self.inicializar_servidor) #Se inicializa el servidor hasta que se oprime el botón.
         self.juego_empezado = False #Un par de banderas para conocer el estado del juego.
         self.juego_pausado = False
         self.timer = None #Se especifica que la ventana tendrá contadores asociados; por ahora no han sido creados.
         self.timer_comida = None
         self.timer_guisantes = None 
+        self.timer_s = None
+        self.timer_camino = None #Este timer estará actualizando la lista de tuplas de las serpientes del juego.
         self.highscore = 0
         self.serpientes_juego = [] #Lista que contendrá todas las serpientes.
         self.guisantes = [] #Lista que contiene las posiciones de la comida de las serpientes.
@@ -42,9 +66,162 @@ class VentanaServidor(QtGui.QMainWindow):
         self.spinBox_2.valueChanged.connect(self.actualizar_tabla) #Asociamos los spinboxes del número de columnas y filas a la función que actualiza el tamaño de la tabla.
         self.spinBox_3.valueChanged.connect(self.actualizar_tabla)
         self.spinBox.valueChanged.connect(self.actualizar_timer) #Y el spinbox que determina la velocidad de actualización de las serpientes también lo asociamos con la función que actualiza el intervalo del timer.
+        self.time.valueChanged.connect(self.actualizar_timeout)
         self.pushButton_2.clicked.connect(self.comenzar_juego) #Finalmente, los botones de la parte inferior se asocian con las funciones que modifican el estado del juego.
         self.pushButton_3.clicked.connect(self.terminar_juego)
         self.show() #Se muestra la ventana.
+
+    def hacer(self):
+        '''
+        No se hace el serve_forever() del servidor
+        para poder regresar a la ejecución del loop principal del juego. 
+        El servidor entonces solamente ejectuta las peticiones que hay en la cola y 
+        esto a partir de su timeout. Después se regresa al loop del juego.
+        '''
+        self.servidor.handle_request()
+
+    def actualizar_camino(self):
+        '''
+        Función que se encarga de formar la lista de tuplas
+        que representa las celdas de la tabla que ocupan todas las 
+        serpientes. Se tiene que asociar con un QTimer dado que las tuplas no 
+        son mutables y entonces esta lista se tendrá que ir reconstruyendo cada
+        cierto lapso.
+        '''
+        for serpiente in self.serpientes_juego:
+            serpiente.camino = []
+            for casilla in serpiente.casillas:
+                #Se construye a partir de la lista de listas, mismas que sí son mutables.
+                serpiente.camino.append((casilla[0], casilla[1]))
+    
+    def inicializar_servidor(self):
+        '''
+        Función encargada de crear al servidor xmlrpc. Se le asigna una dirección
+        y un puerto, además de registrar las funciones a las que tendrán acceso los clientes.
+        '''
+        puerto = self.spinBox_4.value()
+        direccion = self.lineEdit.text()
+        #print(direccion)
+        self.servidor = SimpleXMLRPCServer((direccion, 0)) #El puerto 0 hace que el sistema le asigne un puerto disponible.
+        puerto = self.servidor.server_address[1] 
+        self.spinBox_4.setValue(puerto) #Ponemos en la interfaz el puerto que le fue asignado.
+        self.spinBox_4.setReadOnly(True) #Bloqueamos los spinboxes y botones para no modificar la configuración que ya se le dio.
+        self.lineEdit.setReadOnly(True) #Bloqueamos los spinboxes y botones para no modificar la configuración que ya se le dio.
+        self.pushButton.setEnabled(False)
+        #Se registran las cuatro funciones del servidor.
+        self.servidor.register_function(self.ping)
+        self.servidor.register_function(self.yo_juego)
+        self.servidor.register_function(self.cambia_direccion)
+        self.servidor.register_function(self.estado_del_juego)
+        self.servidor.timeout = 0 #Inicialmente, esta es la espera del servidor (tiempo para ejecutar las peticiones antes de volver al loop del juego).
+        self.timer_s = QtCore.QTimer(self)
+        self.timer_s.timeout.connect(self.hacer) #Para cada contador se le asocia la función correspondiente.
+        self.timer_s.start(self.servidor.timeout) #y se especifica su intervalo.
+
+    
+    def lista_viboras(self):
+        '''
+        Función que regresa una lista con las representaciones de todas las víboras
+        del juego a partir de su representación a partir de un diccionario. 
+        Será usada esta lista en la función asociada al servidor "yo_juego()".
+        '''
+        lista = list()
+        for serpiente in self.serpientes_juego:
+            lista.append(serpiente.obtener_diccionario())
+        return lista
+
+
+    '''Funciones del cliente'''
+    '''''''''''''''''''''''''''
+    '''''''''''''''''''''''''''
+
+    def ping(self):
+        return "¡Pong!"
+
+    def yo_juego(self):
+        '''
+        Permite registrar una nueva serpiente en el juego.
+        Se le asigna en su constructor un color aleatorio, un id único, etc.
+        Posteriormente, también se tiene que regresar la información de la serpiente 
+        que fue creada.
+        '''
+        serpiente_nueva = self.crear_serpiente()
+        diccionario = {"id": serpiente_nueva.id, "color": serpiente_nueva.color}
+        return diccionario
+
+    def cambia_direccion(self, identificador, numero):
+        '''
+        Permite que el cliente pueda modificar la dirección en la que se mueve 
+        una serpiente. Para ésto le tendrá que pasar como parámetros a la función 
+        el identificador de la serpiente cuya dirección será modificada y la dirección 
+        (dado por 0, 1, 2, 3).
+        '''
+        for s in self.serpientes_juego:
+            #Busca que el id dado concuerde con el de una serpiente del juego.
+            if s.id == identificador:
+                #Y de ser posible, modifica la dirección de la serpiente encontrada.
+                if numero == 0:
+                    if s.direccion is not "Abajo": 
+                        s.direccion = "Arriba"
+                if numero == 1:
+                    if s.direccion is not "Izquierda":
+                        s.direccion = "Derecha"
+                if numero == 2: 
+                    if s.direccion is not "Arriba":
+                        s.direccion = "Abajo"
+                if numero == 3: 
+                    if s.direccion is not "Derecha":
+                        s.direccion = "Izquierda"
+        return True #Tienen que forzosamente regresar algo las funciones del servidor.
+
+    def estado_del_juego(self):
+        '''
+        Función del servidor que regresa un diccionario
+        con la información clave del juego al cliente.
+        '''
+        diccionario = dict()
+        diccionario = {
+            'espera': self.servidor.timeout, 
+            'tamX': self.tableWidget.columnCount(),
+            'tamY': self.tableWidget.rowCount(),
+            'viboras': self.lista_viboras() #Esta lista ya fue construída con otra función.
+        }
+        return diccionario
+
+
+
+    def crear_serpiente(self):
+        '''
+        Función para meter una nueva serpiente al juego. 
+        La crea de un tamaño de tres celdas y verifica que estas celdas no estén ocupadas.
+        '''
+        serpiente_nueva = Serpiente()
+        creada = False
+        while not creada:
+            #Crea número aleatorios sin excederse de las dimensiones de la tabla.
+            creada = True
+            uno = randint(1, self.tableWidget.rowCount()/2)
+            dos = uno + 1
+            tres = dos +1 
+            ancho = randint(1, self.tableWidget.columnCount()-1)
+            achecar_1, achecar_2, achecar_3 = [uno, ancho], [dos, ancho], [tres, ancho]
+            for s in self.serpientes_juego:
+                #Si las casillas ya han sido ocupadas, tiene que volver a generar los números.
+                if achecar_1 in s.casillas or achecar_2 in s.casillas or achecar_3 in s.casillas:
+                    creada = False
+                    break
+            #Cuando ya sean válidas, especificamos qué celdas ocupa la víbora.
+            serpiente_nueva.casillas = [achecar_1, achecar_2, achecar_3]
+            self.serpientes_juego.append(serpiente_nueva) #Y la metemos a la lista de las serpientes en el juego.
+            return serpiente_nueva
+
+    def actualizar_timeout(self):
+        '''
+        Función que permite modificar el tiempo que tiene el servidor
+        para llevar a cabo las peticiones de los clientes.
+        '''
+        self.servidor.timeout = self.time.value() #Se moficia el valor del timer asociado a partir del valor del doublespinbox.
+        self.timer_s.setInterval(self.time.value())
 
     def comenzar_juego(self):
         '''
@@ -52,8 +229,7 @@ class VentanaServidor(QtGui.QMainWindow):
         '''
         if not self.juego_empezado:
             self.pushButton_3.show() #Se muestra el botón que permitirá terminar el juego después.
-            serpiente_1 = Serpiente(181,201,70)  #Se crea la serpiente y se añade a las del juego.
-            self.serpientes_juego.append(serpiente_1)
+            self.crear_serpiente() #Por default crearemos una serpiente sin que se pida, para debuggear.
             self.pushButton_2.setText("Pausar el Juego") #Cambia el texto del botón de inicio.
             self.dibujar_serpientes()
             self.timer = QtCore.QTimer(self) #Se crean los tres contadores. El primero es de la actualización de las serpientes, el segundo de la aparición de la comida, y el tercero de la verificación de si la serpiente choca.
@@ -65,6 +241,9 @@ class VentanaServidor(QtGui.QMainWindow):
             self.timer_comida.start(200)
             self.timer_guisantes.timeout.connect(self.crear_guisante) 
             self.timer_guisantes.start(5000) 
+            self.timer_camino = QtCore.QTimer(self)
+            self.timer_camino.timeout.connect(self.actualizar_camino)
+            self.timer_camino.start(100)
             self.tableWidget.installEventFilter(self) #Por medio de esta asociación de un escucha a la tabla, ya no es necesario hacer sublassing del QTableWidget y promoción en QtDesigner.
             self.juego_empezado = True #E indicamos que el juego ha comenzado para futuras modificaciones del estado.
         elif self.juego_empezado and not self.juego_pausado: #Si volvemos a presionar el botón:
@@ -155,7 +334,7 @@ class VentanaServidor(QtGui.QMainWindow):
         '''
         for serpiente in self.serpientes_juego:
             for seccion_corporal in serpiente.casillas:
-                self.tableWidget.item(seccion_corporal[0], seccion_corporal[1]).setBackground(QtGui.QColor(serpiente.color[0], serpiente.color[1], serpiente.color[2]))
+                self.tableWidget.item(seccion_corporal[0], seccion_corporal[1]).setBackground(QtGui.QColor(serpiente.color['r'], serpiente.color['g'], serpiente.color['b']))
     
     def crear_guisante(self):
         '''
@@ -210,6 +389,21 @@ class VentanaServidor(QtGui.QMainWindow):
                 return True
         return False
 
+    def ha_chocado_con_otra_serpiente(self, serpiente_a_checar):
+        '''
+        Verifica si la serpiente chocó con una serpiente.
+        '''
+        for serpiente in self.serpientes_juego:
+            #Solamente tenemos que checar que choque con las OTRAS, no consigo misma.
+            if serpiente.id != serpiente_a_checar.id:
+                for seccion_corporal in serpiente.casillas[:]: 
+                    #Si chocó, con la cabeza, con otra serpiente...
+                    if serpiente_a_checar.casillas[-1][0] == seccion_corporal[0] and serpiente_a_checar.casillas[-1][1] == seccion_corporal[1]:
+                        self.label_7.setText("¡Chocaste!") #Se indica al usuario que la serpiente murió, cambiando del texto del label.
+                        self.label_7.setStyleSheet("QLabel { color : #fc360a; }")
+                        QtCore.QTimer.singleShot(2000, lambda: self.label_7.setText('')) #Después, el label retoma su valor de cadena vacía.
+                        self.serpientes_juego.remove(serpiente_a_checar) #Como la serpiente murió, sale del juego.
+
     def mover_serpientes(self):
         '''
         Simula el movimiento de las serpientes del juego al ir pintando las celdas de acuerdo a la dirección
@@ -218,11 +412,10 @@ class VentanaServidor(QtGui.QMainWindow):
         la cebza de la sepiente.
         '''
         for serpiente in self.serpientes_juego: #Tiene que mover a todas las serpientes.
-            if self.ha_chocado_consigo(serpiente): #Si chocó la serpiente,
+            if self.ha_chocado_consigo(serpiente) or self.ha_chocado_con_otra_serpiente(serpiente): #Si chocó la serpiente,
                 self.serpientes_juego.remove(serpiente) #Se borra de la lista.
                 self.llenar_tabla() #Se tiene que rellenar la tabla.
-                r,g,b = randint(0,255),randint(0,255),randint(0,255) #Y se crea una nueva serpiente de un color generado aleatoriamente.
-                serpiente_1 = Serpiente(r,g,b)
+                serpiente_1 = self.crear_serpiente()
                 self.serpientes_juego = [serpiente_1]
             self.tableWidget.item(serpiente.casillas[0][0],serpiente.casillas[0][1]).setBackground(QtGui.QColor(82,135,135))
             x = 0 #Variable que permite acceder a la siguiente sección coportal de la iteración actual.
